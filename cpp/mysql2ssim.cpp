67de8daaed1d652fd09cc3cd8f0b8cdefcf04734
will chandler <will@withflex.com>
2025-10-28 20:53:55 -0400

    Fix mysqld_error.h header path

--- cpp/mysql2ssim.cpp
+++ cpp/mysql2ssim.cpp
@@ -29,7 +29,7 @@
 #include "include/gen/mysql2ssim_gen.h"
 #include "include/gen/mysql2ssim_gen.inl.h"
 #include "include/lib_mysql.h"
-#include <mariadb/mysqld_error.h> // for ER_BAD_DB_ERROR
+#include <mysqld_error.h> // for ER_BAD_DB_ERROR
 
 // -----------------------------------------------------------------------------
 
